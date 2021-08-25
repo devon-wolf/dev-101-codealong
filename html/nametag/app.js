9a3f41f540a27290b0a32eea6f804a3e18c2ff4f
Devon Wolfkiel <devon.wolfkiel@gmail.com>
2021-08-25 11:30:28 -0700

    Add input to name tag

--- html/nametag/app.js
+++ html/nametag/app.js
@@ -1,2 +1,9 @@
-bad.code
-"a string"
+const displayName = document.getElementById('display-name');
+const nameForm = document.querySelector('form');
+const nameInput = document.querySelector('input');
+
+nameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    displayName.textContent = nameInput.value;
+    nameInput.value = '';
+});
